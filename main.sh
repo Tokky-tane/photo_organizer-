6018858a5f07e95073f3cb55bba4ad77a0743a15
Tokky_tane <tokky_tane@outlook.com>
2021-11-21 18:06:18 +0900

    dry runのオプションを追加

--- main.sh
+++ main.sh
@@ -1,5 +1,25 @@
 set -eu
+
+dry_run=0
+
+while getopts d OPT; do
+  case $OPT in
+    d)
+      dry_run=1
+      ;;
+  esac
+done
+shift $((OPTIND - 1))
+
 target_d=$1
+
+function run_cmd() {
+  if [ $dry_run -eq 1 ]; then
+    echo "dry run: $@"
+  else
+    $@
+  fi
+}
 
 while read path; do
   dir=$(
@@ -7,7 +27,10 @@ while read path; do
       awk '{print $4}' |
       awk -F ':' '{ printf "%s-%s", $1, $2 }'
   )
-  if [ ! -e ${target_d}/${dir} ]; then mkdir -v ${target_d}/${dir}; fi
+  if [ ! -e ${target_d}/${dir} ]; then run_cmd mkdir -v ${target_d}/${dir}; fi
   echo -n . 1>&2
-  cp -vn ${path} ${target_d}/${dir}/
+
+  if [ ! -e ${target_d}/${dir}/$(basename $path) ]; then
+    run_cmd cp -v $path ${target_d}/${dir}
+  fi
 done
